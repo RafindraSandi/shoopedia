8a970d9d55c0318da79b644991594948ad7245e1
RafindraSandi <149604286+RafindraSandi@users.noreply.github.com>
2026-01-05 15:01:47 +0700

    Update product.dart

--- lib/pages/models/product.dart
+++ lib/pages/models/product.dart
@@ -1,3 +1,37 @@
+class Review {
+  final String name;
+  final int rating;
+  final String comment;
+  final String date;
+
+  Review({
+    required this.name,
+    required this.rating,
+    required this.comment,
+    required this.date,
+  });
+
+  // Convert Review to Map for storage
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'rating': rating,
+      'comment': comment,
+      'date': date,
+    };
+  }
+
+  // Create Review from Map
+  factory Review.fromMap(Map<String, dynamic> map) {
+    return Review(
+      name: map['name'] ?? '',
+      rating: map['rating'] ?? 0,
+      comment: map['comment'] ?? '',
+      date: map['date'] ?? '',
+    );
+  }
+}
+
 class Product {
   final String name;
   final String image;
@@ -6,6 +40,8 @@ class Product {
   final String description;
   final List<String> sizes;
   final String? discount;
+  final String storeName;
+  final List<Review> reviews;
 
   Product({
     required this.name,
@@ -15,5 +51,54 @@ class Product {
     required this.description,
     required this.sizes,
     this.discount,
+    required this.storeName,
+    this.reviews = const [],
   });
+
+  // Convert Product to Map for storage
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'image': image,
+      'price': price,
+      'sold': sold,
+      'description': description,
+      'sizes': sizes,
+      'discount': discount,
+      'storeName': storeName,
+      'reviews': reviews.map((review) => review.toMap()).toList(),
+    };
+  }
+
+  // Create Product from Map
+  factory Product.fromMap(Map<String, dynamic> map) {
+    return Product(
+      name: map['name'] ?? '',
+      image: map['image'] ?? '',
+      price: map['price'] ?? '',
+      sold: map['sold'] ?? '',
+      description: map['description'] ?? '',
+      sizes: List<String>.from(map['sizes'] ?? []),
+      discount: map['discount'],
+      storeName: map['storeName'] ?? '',
+      reviews: map['reviews'] != null
+          ? List<Review>.from(map['reviews'].map((x) => Review.fromMap(x)))
+          : [],
+    );
+  }
+
+  // Method to add a review
+  Product addReview(Review review) {
+    return Product(
+      name: name,
+      image: image,
+      price: price,
+      sold: sold,
+      description: description,
+      sizes: sizes,
+      discount: discount,
+      storeName: storeName,
+      reviews: [...reviews, review],
+    );
+  }
 }
